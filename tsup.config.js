21b1cc7e72fe592066812e9e21542b4e95058f38
Mike Rourke <mike.w.rourke@gmail.com>
2024-03-01 15:08:28 -0800

    build: switch to `tsup` for building/bundling

---
+++ tsup.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig(() => {
+  const commonOptions = {
+    clean: true,
+    entry: ["src/index.ts"],
+    minify: false,
+    sourcemap: true,
+    tsconfig: "./tsconfig.build.json",
+  };
+
+  return [
+    {
+      ...commonOptions,
+      dts: true,
+      format: "esm",
+      outExtension: () => ({ js: ".mjs" }),
+    },
+    {
+      ...commonOptions,
+      format: "cjs",
+      outExtension: () => ({ js: ".cjs" }),
+    },
+  ];
+});
