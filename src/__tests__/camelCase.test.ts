85e195cf7a18fc4d8b8b060216c8a13ff4324469
Mike Rourke <mike.w.rourke@gmail.com>
2024-11-30 13:48:37 -0600

    refactor: update `camelCase` implementation to handle more string casing

--- src/__tests__/camelCase.test.ts
+++ src/__tests__/camelCase.test.ts
@@ -18,14 +18,34 @@ describe.concurrent("the camelCase function", () => {
   });
 
   it("handles strings starting with a hyphen", async () => {
-    expect(camelCase("-webkit-transition")).toBe("WebkitTransition");
+    expect(camelCase("-webkit-transition")).toBe("webkitTransition");
   });
 
   it("returns an empty string if input is empty", async () => {
     expect(camelCase("")).toBe("");
   });
 
+  it("handles TitleCase strings", async () => {
+    expect(camelCase("TitleCase")).toBe("titleCase");
+  });
+
+  it("handles strings with last character uppercase", async () => {
+    expect(camelCase("some-value___AAA")).toBe("someValueAaa");
+  });
+
+  it("handles single characters", async () => {
+    expect(camelCase("_")).toBe("");
+    expect(camelCase("A")).toBe("a");
+  });
+
   it("handles single hyphen correctly", async () => {
     expect(camelCase("-")).toBe("");
+  });
+
+  it("throws an error if the input value is not a string", async () => {
+    expect(() => {
+      // @ts-ignore
+      camelCase(24);
+    }).toThrow(TypeError);
   });
 });
--- src/camelCase.ts
+++ src/camelCase.ts
@@ -1,9 +1,9 @@
 // noinspection SpellCheckingInspection
 
 /*
- * The MIT License (MIT)
+ * MIT License
  *
- * Copyright (c) Steven Vachon <contact@svachon.com> (svachon.com)
+ * Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (https://sindresorhus.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,23 +24,115 @@
  * THE SOFTWARE.
  */
 
+const UPPERCASE = /[\p{Lu}]/u;
+const LOWERCASE = /[\p{Ll}]/u;
+const IDENTIFIER = /([\p{Alpha}\p{N}_]|$)/u;
+const SEPARATORS = /[_.\- ]+/;
+
+const LEADING_SEPARATORS = new RegExp("^" + SEPARATORS.source);
+// prettier-ignore
+const SEPARATORS_AND_IDENTIFIER = new RegExp(SEPARATORS.source + IDENTIFIER.source, "gu");
+const NUMBERS_AND_IDENTIFIER = new RegExp("\\d+" + IDENTIFIER.source, "gu");
+
+const preserveCamelCase = (value: string): string => {
+  let isLastCharLower = false;
+  let isLastCharUpper = false;
+  let isLastLastCharUpper = false;
+
+  for (let index = 0; index < value.length; index++) {
+    const character = value[index];
+
+    if (isLastCharLower && UPPERCASE.test(character)) {
+      value = value.slice(0, index) + "-" + value.slice(index);
+      isLastCharLower = false;
+      isLastLastCharUpper = isLastCharUpper;
+      isLastCharUpper = true;
+
+      index++;
+
+      continue;
+    }
+
+    /* istanbul ignore next -- @preserve: Vendored, no need to test. */
+    if (isLastCharUpper && isLastLastCharUpper && LOWERCASE.test(character)) {
+      value = value.slice(0, index - 1) + "-" + value.slice(index - 1);
+
+      isLastLastCharUpper = isLastCharUpper;
+      isLastCharUpper = false;
+      isLastCharLower = true;
+
+      continue;
+    }
+
+    isLastCharLower =
+      character.toLowerCase() === character &&
+      character.toUpperCase() !== character;
+
+    isLastLastCharUpper = isLastCharUpper;
+
+    isLastCharUpper =
+      character.toUpperCase() === character &&
+      character.toLowerCase() !== character;
+  }
+
+  return value;
+};
+
+/* istanbul ignore next -- @preserve: Vendored, no need to test. */
+const postProcess = (value: string): string => {
+  SEPARATORS_AND_IDENTIFIER.lastIndex = 0;
+  NUMBERS_AND_IDENTIFIER.lastIndex = 0;
+
+  return value
+    .replaceAll(NUMBERS_AND_IDENTIFIER, (match, pattern, offset) =>
+      ["_", "-"].includes(value.charAt(offset + match.length))
+        ? match
+        : match.toUpperCase(),
+    )
+    .replaceAll(SEPARATORS_AND_IDENTIFIER, (match, identifier) =>
+      identifier.toUpperCase(),
+    );
+};
+
 /**
  * Transforms the specified string value to camelCase. This was taken from the
- * [camelcase-css](https://github.com/stevenvachon/camelcase-css) package
- * and types were added.
+ * [camelcase](https://github.com/sindresorhus/camelcase) package. Type
+ * annotations were added and options were removed.
  *
  * @param value Value to transform.
  *
  * @returns The specified `value` converted to camelCase.
  *
+ * @throws [TypeError](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypeError) if the value is not a string.
+ *
  * @category String
  */
 export const camelCase = (value: string): string => {
-  // RegExp for word boundaries or dashes:
-  const pattern = /-(\w|$)/g;
+  // noinspection SuspiciousTypeOfGuard
+  if (typeof value !== "string") {
+    throw new TypeError("Expected the value to be string");
+  }
 
-  // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_function_as_the_replacement
-  const replacer = (dash: string, char: string): string => char.toUpperCase();
+  let casedValue = value.trim();
 
-  return value.toLowerCase().replace(pattern, replacer);
+  if (casedValue.length === 0) {
+    return "";
+  }
+
+  if (casedValue.length === 1) {
+    if (SEPARATORS.test(casedValue)) {
+      return "";
+    }
+  }
+
+  const hasUpperCase = casedValue !== casedValue.toLowerCase();
+
+  if (hasUpperCase) {
+    casedValue = preserveCamelCase(casedValue);
+  }
+
+  casedValue = casedValue.replace(LEADING_SEPARATORS, "");
+  casedValue = casedValue.toLowerCase();
+
+  return postProcess(casedValue);
 };
